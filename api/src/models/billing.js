0d6e9a518ddac1d738db00084ca8a1ed83c03e46
João Henrique <jabreu@graphite.com>
2021-03-29 19:59:25 -0300

    Moving files around

--- api/src/models/billing.js
+++ api/src/models/billing.js
@@ -1,20 +1,20 @@
 const db = require('mongoose');
 const BaseRepository = require('./repositories/base');
 const baseSchemaOptions = require('./schemas/options');
-const paymentSchema = require('./schemas/payment').schema;
+const paymentSchema = require('../schemas/payment').schema;
 const { BillingData } = require('../config/data');
 
 const { Schema } = db;
 
 const billingSchema = new Schema({
   presentation: { 
-    id: {type: String, required: true}
+    id: { type: String, required: true }
   },
   artist: { 
-    id: {type: String, required: true},
+    id: { type: String, required: true },
   },
   contractor: { 
-    id: {type: String, required: true}
+    id: { type: String, required: true }
   },
   total_amount: { type: Number, required: true },
   total_paid: { type: Number, default: 0 },
